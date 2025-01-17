73ee05659d055f96f4886d9b6c8fc853d6fc6506
Yi-Ting Chiu <mytim710@gmail.com>
2025-01-17 13:35:00 -0700

    added configuration diagram

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -31,6 +31,13 @@ const config: Config = {
     locales: ["en"],
   },
 
+  themes: ['@docusaurus/theme-mermaid'],
+  // In order for Mermaid code blocks in Markdown to work,
+  // you also need to enable the Remark plugin with this option
+  markdown: {
+    mermaid: true,
+  },
+
   presets: [
     [
       "classic",
